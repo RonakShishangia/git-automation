3a83b1cb2cab22f23a80a18e932cf28137df9683
Ronak Shishangia <ronak.nkonn@gmail.com>
2024-07-08 04:07:00 +0530

    function format(inputString) {         if (!inputString) {             inputString = this.isUtc()                 ? hooks.defaultFormatUtc                 : hooks.defaultFormat;         }         var output = formatMoment(this, inputString);         return this.localeData().postformat(output);     }T04:07

--- index.js
+++ index.js
@@ -8,15 +8,29 @@ const commitCode = n => {
     if (n === 0) return simpleGit().push();
     const x = Math.floor(Math.random() * 54) + 1 //random.int(0, 54);
     const y = Math.floor(Math.random() * 6) + 1 //random.int(0, 6); 
-    DATE = moment().subtract(1, "y").add(1, "d").add(x, 'w').add(y, 'd').format();
-    const obj = [{ DATE }];
+    DATE = moment().subtract(1, "y").add(1, "d").add(x, 'w').add(y, 'd').format;
+
+    // Generate random hours (0-23) and minutes (0-59)
+    const randomHours = Math.floor(Math.random() * 24);
+    const randomMinutes = Math.floor(Math.random() * 60);
+
+    // Format time with leading zeros (optional)
+    const formattedHours = randomHours.toString().padStart(2, '0');
+    const formattedMinutes = randomMinutes.toString().padStart(2, '0');
+    
+    const randomTime = `${formattedHours}:${formattedMinutes}`;
+    // Combine date and time (format can be customized)
+    const formattedDateTime = DATE + 'T' + randomTime;
+
+    const obj = [{ DATE: formattedDateTime }];
     jsonfile.writeFile(file, obj, (err) => {
         if (err) console.log("file write error: ", err);
-        jsonfile.readFile(file, (err, data) => {
-            console.log("File data:", data);
-        });
-        // simpleGit().add('./*').commit(DATE, {'--date': DATE}).push();
-        simpleGit().add('./*').commit(DATE, {'--date': DATE}, commitCode.bind(this, --n));
+        console.log("Data: ", obj);
+        // jsonfile.readFile(file, (err, data) => {
+        //     console.log("File data:", data);
+        // });
+        // simpleGit().add('./*').commit(DATE, {'--date': formattedDateTime}).push();
+        simpleGit().add('./*').commit(formattedDateTime, {'--date': formattedDateTime}, commitCode.bind(this, --n));
     });
 }
 
