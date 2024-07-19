741ebf11fe647613d115975cddcbda88e63df62a
Ronak Shishangia <ronak.nkonn@gmail.com>
2024-07-19 16:20:24 +0530

    2024-07-19T16:20:24+05:30

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const moment = require('moment');
 const simpleGit = require('simple-git');
 const jsonfile = require('jsonfile')
-// const fs = require('fs')
 
-DATE = moment().format();
-file = 'data.json';
+file = './data.json';
 
-const obj = [
-    {
-        DATE
-    }
-];
-jsonfile.writeFile(file, obj, (err) => {
-    if (err) console.log("file write error: ", err);
-    const fileData = jsonfile.readFile(file, (err, data) => {
-        console.log("File data1:", data);
-        // data.push({DATE})
-        // jsonfile.writeFile(file, data)
+const commitCode = n => {
+    if (n === 0) return simpleGit().push();
+    const x = Math.floor(Math.random() * 54) + 1 //random.int(0, 54);
+    const y = Math.floor(Math.random() * 6) + 1 //random.int(0, 6); 
+    DATE = moment().subtract(1, "y").add(1, "d").add(x, 'w').add(y, 'd').format();
+    const obj = [{ DATE }];
+    jsonfile.writeFile(file, obj, (err) => {
+        if (err) console.log("file write error: ", err);
+        jsonfile.readFile(file, (err, data) => {
+            console.log("File data:", data);
+        });
+        // simpleGit().add('./*').commit(DATE, {'--date': DATE}).push();
+        simpleGit().add('./*').commit(DATE, {'--date': DATE}, commitCode.bind(this, --n));
     });
-});
+}
 
-// git commit
-simpleGit().add([file]).commit(DATE, {'--date': DATE});
+commitCode(100);
